91f004b795b1b248ac8a30d9a79d03d00da69d5f
Jahanvi Chaudhary <91944643+s0ur-oranges@users.noreply.github.com>
2023-08-27 18:01:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,8 @@ srtfilename = "subtitles.txt"
 mp4filename = "video.mp4"
 
 import sys
-import pysrt
 import os
 import subprocess
-from moviepy.editor import VideoFileClip, TextClip, CompositeVideoClip
 import streamlit as st
 from faster_whisper import WhisperModel
 import time
@@ -23,26 +21,6 @@ def save_uploadedfile(uploadedfile):
 
 def time_to_seconds(time_obj):
     return time_obj.hours * 3600 + time_obj.minutes * 60 + time_obj.seconds + time_obj.milliseconds / 1000
-
-
-def create_subtitle_clips(subtitles, videosize,fontsize=24, font='Arial', color='yellow', debug = False):
-    subtitle_clips = []
-
-    for subtitle in subtitles:
-        start_time = time_to_seconds(subtitle.start)
-        end_time = time_to_seconds(subtitle.end)
-        duration = end_time - start_time
-
-        video_width, video_height = videosize
-
-        text_clip = TextClip(subtitle.text, fontsize=fontsize, font=font, color=color, bg_color = 'black',size=(video_width*3/4, None), method='caption').set_start(start_time).set_duration(duration)
-        subtitle_x_position = 'center'
-        subtitle_y_position = video_height* 4 / 5
-
-        text_position = (subtitle_x_position, subtitle_y_position)
-        subtitle_clips.append(text_clip.set_position(text_position))
-
-    return subtitle_clips
 
 def video2mp3(video_file, output_ext="mp3"):
     filename, ext = os.path.splitext(video_file)
